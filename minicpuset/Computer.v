d339e043196f3b0349fa746ae825fc1b6c0f1e7b
pirrot <uchiyama.kazuhide@gmail.com>
2019-05-19 21:01:12 +0900

    Yunomi is yoi

---
+++ minicpuset/Computer.v
@@ -0,0 +1,11 @@
+module Computer(reset,clock);
+    input reset,clock;
+    wire writeM;
+    wire [15:0] outM,inM,instruction;
+    wire [14:0] addressM,pc_;
+
+
+    RAM32K RAM32K(clock,outM,writeM,addressM,inM);
+    miniCPU miniCPU(inM,instruction,reset,outM,writeM,addressM,pc_,clock);
+    ROM32K ROM32K(clock,pc_,instruciton);
+endmodule
--- minicpuset/ROM32K.v
+++ minicpuset/ROM32K.v
@@ -1,10 +1,9 @@
-module ROM32K(clock,in,address,out);
+module ROM32K(clock,address,out);
     input clock;
     input [14:0] address;
-    input [15:0] in;
     output [15:0] out;
 
     reg [15:0] out;
 
-    RAM32K RAM32K(clock,in,1'b0,address,out);
+    RAM32K RAM32K(clock,16'b0000000000000000,1'b0,address,out);
 endmodule
